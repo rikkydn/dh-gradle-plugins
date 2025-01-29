1091a2d930a9bb90bb27551d527021ab3413ba02
Dzmitry Halubovich <external.dzmitry.halubovich@kuehne-nagel.com>
2025-01-29 12:58:41 +0000

    init

--- plugin/jacoco/build.gradle.kts
+++ plugin/jacoco/build.gradle.kts
@@ -7,20 +7,6 @@ gradlePlugin {
         id = "by.dh.jacoco"
         displayName = "DH Jacoco Plugin"
         implementationClass = "by.dh.plugin.jacoco.JacocoPlugin"
-        website = "https://github.com/rikkydn/dh-gradle-plugins"
-        vcsUrl = "https://github.com/rikkydn/dh-gradle-plugins"
-        description = "Preconfigured jacoco plugin"
-        tags.set(listOf("jacoco"))
-    }
-    repositories {
-        maven {
-            name = "dh.gradle.plugin"
-            url = uri("https://maven.pkg.github.com/rikkydn/dh-gradle-plugins")
-            credentials {
-                username = System.getenv("DH_GH_USERNAME")
-                password = System.getenv("DH_GH_TOKEN")
-            }
-        }
     }
 }
 
